f55ac33dc64db6935877ea543fa22ce0eeb2b44a
Amanjeet Sharma <amansharma23503@gmail.com>
2025-05-27 21:54:15 +0530

    feat: enhance signUp and signIn functions with improved user validation and response handling

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -33,25 +33,68 @@ export const signUp = async (req, res, next) => {
             { expiresIn: JWT_EXPIRES_IN }
         )
 
+        const isUserCreated = await User.findById(newUser[0]._id)
+            .select("-password -token") // Exclude password and token from the response
+            .session(session); // 👈 This is required inside the transaction
+
+
         // Commit the transaction
         await session.commitTransaction();
-        session.endSession();
 
+        console.log("✅  User created successfully");
         // Respond with success
         res.status(201).json({
             success: true,
             message: "User created successfully",
-            user: newUser[0],
+            user: isUserCreated,
             token
         });
 
     } catch (error) {
         await session.abortTransaction();
+        next(error);
+    } finally {
         session.endSession();
+    }
+
+}
+
+export const signIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        // Find user by email
+        const user = await User.find({ email });
+        if (!user) {
+            const error = new Error("User not found");
+            error.status = 404; // Not Found
+            throw error;
+        }
+        const isPasswordValid = await bcrypt.compare(password, user[0].password);
+        if (!isPasswordValid) {
+            const error = new Error("Invalid password");
+            error.status = 401; // Unauthorized
+            throw error;
+        }
+
+        const token = jwt.sign({ userId: user._id },
+            JWT_SECRET,
+            { expiresIn: JWT_EXPIRES_IN }
+        );
+
+        const safeUser = await User.findById(user[0]._id)
+            .select("-password");
+
+        console.log("✅  User signed in successfully");
+        res.status(200).json({
+            success: true,
+            message: "User signed in successfully",
+            user: safeUser,
+            token
+        });
+
+    } catch (error) {
         next(error);
     }
 }
 
-export const signIn = async (req, res, next) => { }
-
-export const signOut = async (req, res, next) => { }
+export const signOut = async (req, res, next) => {}
